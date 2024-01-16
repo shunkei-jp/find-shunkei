6738243595ec5a12409b2f0d6e4283fc58fa1073
Ebihara Yusuke <yusuke@ebihara.me>
2024-01-17 05:40:10 +0900

    refactor: define Lookup func and print in main

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"net"
 	"sync"
 	"time"
 
@@ -11,22 +13,49 @@ import (
 )
 
 func main() {
+	timeout := flag.Int("t", 1, "timeout")
+	flag.Parse()
+
 	fmt.Printf("IPv4 \thostname \tservice\n")
 
 	var queryes = []string{"_shunkei_vtx_tx._tcp", "_shunkei_vtx_rx._tcp"}
+
+	resultsChan := make(chan LookupResult)
 
 	var wg sync.WaitGroup
 	for _, query := range queryes {
 		wg.Add(1)
 		go func(query string) {
 			defer wg.Done()
-			lookup(query)
+			err := Lookup(resultsChan, query, *timeout)
+			if err != nil {
+				log.Fatalln("Failed to lookup:", err.Error())
+			}
 		}(query)
 	}
-	wg.Wait()
+	done := make(chan bool)
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		fmt.Println("done")
+	case result := <-resultsChan:
+		fmt.Printf("%v \t%v \t%v\thttp://%v/\n", result.ipv4, result.hostname, result.service, result.ipv4)
+	}
+
+	<-done
 }
 
-func lookup(query string) {
+type LookupResult struct {
+	ipv4     net.IP
+	hostname string
+	service  string
+}
+
+func Lookup(resultsChan chan<- LookupResult, query string, timeout int) error {
 	// Discover all services on the network (e.g. _workstation._tcp)
 	resolver, err := zeroconf.NewResolver(nil)
 	if err != nil {
@@ -37,17 +66,19 @@ func lookup(query string) {
 	go func(results <-chan *zeroconf.ServiceEntry) {
 		for entry := range results {
 			for _, addr := range entry.AddrIPv4 {
-				fmt.Printf("%v \t%v \t%v\thttp://%v/\n", addr, entry.HostName, query, addr)
+				resultsChan <- LookupResult{ipv4: addr, hostname: entry.HostName, service: query}
 			}
 		}
 	}(entries)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
 	defer cancel()
 	err = resolver.BrowseWithStrategy(ctx, query, "local.", zeroconf.ForceIPv4, entries)
 	if err != nil {
-		log.Fatalln("Failed to browse:", err.Error())
+		return err
 	}
 
 	<-ctx.Done()
+
+	return nil
 }
